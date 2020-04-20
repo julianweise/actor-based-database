0d554744f43c9b37cc113d9b9ed2ee38e2fa39a8
Julian Weise <privat@julianweise.de>
2020-04-20 09:04:34 +0200

    add equals method to ADBKeyPair

--- src/main/java/de/hpi/julianweise/utility/largemessage/ADBKeyPair.java
+++ src/main/java/de/hpi/julianweise/utility/largemessage/ADBKeyPair.java
@@ -14,4 +14,12 @@ public class ADBKeyPair {
         this.key = a;
         this.value = b;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof ADBKeyPair)) {
+            return false;
+        }
+        return ((ADBKeyPair) o).key == this.key && ((ADBKeyPair) o).value == this.value;
+    }
 }
