3bd79d8fbf97f17a6902f1546a29a0074dee9b2e
Julian Weise <privat@julianweise.de>
2020-08-07 11:58:27 +0200

    fix mixed up execution and target nodes

--- src/main/java/de/hpi/julianweise/utility/query/join/JoinExecutionPlan.java
+++ src/main/java/de/hpi/julianweise/utility/query/join/JoinExecutionPlan.java
@@ -161,7 +161,7 @@ public class JoinExecutionPlan extends AbstractBehavior<JoinExecutionPlan.Comman
     private void stealWork(GetNextJoinNodePair command) {
         int targetNodeId = this.history.getLastNodeHandlingAJoin(partitionManagers.indexOf(command.requestingManager));
         if (targetNodeId > -1) {
-            command.responseTo.tell(new StealWork(partitionManagers.get(targetNodeId), command.requestingManager));
+            command.responseTo.tell(new StealWork(command.requestingManager, partitionManagers.get(targetNodeId)));
             this.history.logWorkStealing(this.partitionManagers.indexOf(command.requestingManager), targetNodeId);
         } else {
             command.responseTo.tell(NextJoinNodePair.builder()
