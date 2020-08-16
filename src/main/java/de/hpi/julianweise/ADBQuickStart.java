af8cda390ae9ed1734987c95ef55d74665cb7b36
Julian Weise <privat@julianweise.de>
2020-08-16 14:02:15 +0200

    activate cloud

--- src/main/java/de/hpi/julianweise/ADBQuickStart.java
+++ src/main/java/de/hpi/julianweise/ADBQuickStart.java
@@ -1,6 +1,6 @@
 package de.hpi.julianweise;
 
-import de.hpi.julianweise.domain.custom.flight.FlightFactory;
+import de.hpi.julianweise.domain.custom.cloudobservation.CloudObservationFactory;
 import de.hpi.julianweise.slave.partition.data.ADBEntityFactory;
 import de.hpi.julianweise.slave.partition.data.comparator.ADBComparator;
 
@@ -13,6 +13,6 @@ public class ADBQuickStart {
     }
 
     private static ADBEntityFactory createEntityFactory() {
-        return new FlightFactory();
+        return new CloudObservationFactory();
     }
 }
