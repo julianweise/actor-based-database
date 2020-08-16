9e4cf13477a72e794a512aa77d3eb8053a1eedda
Julian Weise <privat@julianweise.de>
2020-08-16 17:51:23 +0200

    activate Flight dataset

--- src/main/java/de/hpi/julianweise/ADBQuickStart.java
+++ src/main/java/de/hpi/julianweise/ADBQuickStart.java
@@ -1,5 +1,6 @@
 package de.hpi.julianweise;
 
+import de.hpi.julianweise.domain.custom.flight.FlightFactory;
 import de.hpi.julianweise.domain.custom.tpch.LineItemAndPartFactory;
 import de.hpi.julianweise.slave.partition.data.ADBEntityFactory;
 import de.hpi.julianweise.slave.partition.data.comparator.ADBComparator;
@@ -13,6 +14,6 @@ public class ADBQuickStart {
     }
 
     private static ADBEntityFactory createEntityFactory() {
-        return new LineItemAndPartFactory();
+        return new FlightFactory();
     }
 }
